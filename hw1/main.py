e9831b128105681039b11a0c47fbede152fe3cbc
Raj Chopra <raj.chopra@vanderbilt.edu>
2024-01-29 13:02:58 -0600

    create node class

---
+++ hw1/main.py
@@ -0,0 +1,6 @@
+class Node: 
+    def __init__(self): 
+        self.name = ''
+        # self.children stores adjacent nodes via the following interfact (Node, Edge Preference) <- that's a tuple
+        self.children = []
+    

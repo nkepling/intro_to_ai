487b30ee43de4137b25d28b30e1d1fd8310c9456
Raj Chopra <raj.chopra@vanderbilt.edu>
2024-01-29 13:04:45 -0600

    add len to edge

--- hw1/main.py
+++ hw1/main.py
@@ -1,6 +1,6 @@
 class Node: 
     def __init__(self): 
         self.name = ''
-        # self.children stores adjacent nodes via the following interfact (Node, Edge Preference) <- that's a tuple
+        # self.children stores adjacent nodes via the following interfact (Node, Length, Edge Preference) <- that's a tuple
         self.children = []
     
